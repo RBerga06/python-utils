cb2828d03d63274cf9ad831cca48cb3734fcae13
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-04-11 17:18:09 +0200

    Made `rberga06.utils` a namespace package

---
+++ src/rberga06/utils/__about__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""RBerga06's Python Utils"""
+from .about import about as about
+
+__about__ = about("rberga06-utils", "rberga06.utils")
+__version__ = __about__.version
+
+__all__ = [
+    "__about__",
+    "__version__",
+]
