6fa058af877c680d1707c6164f304ce1d60f642c
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-05-15 22:12:50 +0200

    Improved wrap.py

--- src/rberga06/utils/decorators/wrap.py
+++ src/rberga06/utils/decorators/wrap.py
@@ -5,15 +5,13 @@
 # Inspired by the wrapt library
 
 from contextlib import suppress
-from inspect import signature
 from typing import Callable as Fn
 from typing import Any, Sequence, cast
-from typing_extensions import TypeVar, ParamSpec
+from typing_extensions import TypeVar
+from .sig import sig
 
 
 _F = TypeVar("_F", infer_variance=True, bound = Fn[..., Any], default = Fn[..., Any])
-_P = ParamSpec("_P", default=...)
-_R = TypeVar("_R", default=Any)
 
 
 WRAPPER_ASSIGNMENTS = (
@@ -23,15 +21,6 @@ WRAPPER_ASSIGNMENTS = (
 WRAPPER_UPDATES = (
     "__dict__",
 )
-
-
-def copysignature(src: _F, /, *, runtime: bool = True) -> Fn[[Fn[..., Any]], _F]:
-    def inner(dst: Fn[..., Any], /) -> _F:
-        if runtime:
-            with suppress(AttributeError):
-                object.__setattr__(dst, "__signature__", signature(src))
-        return cast(_F, dst)
-    return inner
 
 
 def update_wrapper(
@@ -50,7 +39,7 @@ def update_wrapper(
             cast(dict[str, Any], object.__getattribute__(wrapper, attr)).update(
                 cast(dict[str, Any], object.__getattribute__(wrapped, attr))
             )
-    return copysignature(wrapped, runtime=False)(wrapper)
+    return sig[wrapped].typing(wrapper)
 
 
 def wraps(
@@ -62,12 +51,3 @@ def wraps(
     def inner(wrapper: Fn[..., Any], /) -> _F:
         return update_wrapper(wrapper, wrapped, assigned=assigned, updated=updated)
     return inner
-
-
-
-@wraps(int)
-def int2(*args: Any, **kwargs: Any) -> int:
-    return int(*args, **kwargs)
-
-print(int, int.__doc__)
-print(int2, int2.__doc__)
