e96aa983b7f7581624d34dd23c139cd0c052a34e
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-05-28 18:02:09 +0200

    First tests for func.sig

---
+++ tests/test_func.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Tests for rberga06.utils.func ns package."""
+from rberga06.utils.func.sig import sig
+
+
+def foo(x: int, /, y: bool = True, *, z: list[str] | None = None) -> str:
+    return str(len(z) if z and y else x)
+
+
+class TestSig:
+    def test_sig(self) -> None:
+        assert sig[foo]
