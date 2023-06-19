f0721aa78713275998c798de3cd8485f80e58a5a
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-06-19 11:14:18 +0200

    Implemented logging behaviour

--- src/rberga06/utils/func/info.py
+++ src/rberga06/utils/func/info.py
@@ -2,20 +2,33 @@
 # -*- coding: utf-8 -*-
 # mypy: ignore-errors
 """Retrieve detailed information about function calls."""
+from collections.abc import Iterable
 from typing import Any, ClassVar, Generic, NamedTuple, TypeVar, cast
 from typing_extensions import override
 from .dec import AnyFn, DecoratorWithAttr
-from .info import CallInfo
+import logging
 
 # Type variables
 _F = TypeVar("_F", bound=AnyFn)
 # Type aliases
-FrozenArgs = tuple[Any, ...]
+FrozenArgs   = tuple[Any, ...]
 FrozenKwargs = frozenset[tuple[str, Any]]
 
 
 def freeze_params(*args: Any, **kwargs: Any) -> tuple[FrozenArgs, FrozenKwargs]:
     return args, frozenset(kwargs.items())
+
+
+def _f_full_name(f: AnyFn) -> str:
+    module, qualname = f.__module__, f.__qualname__
+    if module == "__main__":
+        return qualname
+    return f"{module}:{qualname}"
+
+
+def _desc_f_call(f: AnyFn, args: Iterable[Any], kwargs: Iterable[tuple[str, Any]]) -> str:
+    params = [*map(repr, args), *[f"{k}={v}" for k, v in kwargs]]
+    return f"{_f_full_name(f)}({', '.join(params)})"
 
 
 class CallInfo(NamedTuple, Generic[_F]):
@@ -25,16 +38,32 @@ class CallInfo(NamedTuple, Generic[_F]):
     result: Any
     success: bool
 
+    def describe(self) -> str:
+        result = f"{self.result!r}" if self.success else f"(!) {type(self.result).__qualname__} (!)"
+        return f"{_desc_f_call(self.func, self.args, self.kwargs)} -> {result}"
+
+    @override
+    def __repr__(self) -> str:
+        return f"<CallInfo: {self.describe()}>"
+
 
 class call_info(DecoratorWithAttr[list[CallInfo[AnyFn]]]):
     ATTR: ClassVar[str] = "call_info"
+    log: bool
+    logger: logging.Logger | None
 
-    def __init__(self, /) -> None:
+    def __init__(self, /, *, log: bool = True, logger: logging.Logger | None = None) -> None:
         super().__init__([])
+        self.log = log
+        self.logger = logger
 
     @override
     def spec(__self__, __decorated__: AnyFn, *args: Any, **kwargs: Any) -> Any:
         """Decorator behaviour specification."""
+        if __self__.log:
+            (__self__.logger or logging.getLogger()).debug(
+                f"-> {_desc_f_call(__decorated__, args, kwargs.items())}"
+            )
         try:
             result = __decorated__(*args, **kwargs)
         except BaseException as err:
@@ -53,6 +82,11 @@ class call_info(DecoratorWithAttr[list[CallInfo[AnyFn]]]):
                 *freeze_params(*args, **kwargs),
                 result, success
             ))
+
+    @override
+    def decorate(self, f: _F) -> _F:
+        self.logger = logging.getLogger(_f_full_name(f))
+        return super().decorate(f)
 
     @classmethod
     @override
